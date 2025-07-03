98769fce3af7bc5b9ae6c771f10d191e5bbedf32
setiya04 <setiyawati@mhs.itenas.ac.id>
2025-07-03 15:14:35 +0700

    added roast

---
+++ src/data.js
@@ -0,0 +1,102 @@
+export const memes = [
+    {
+        id: 1,
+        text: "Gitu doang nih, {name}? Gue kira hidup lo penuh plot twist, ternyata isinya filler doang 😜.",
+        meme: "https://i.giphy.com/bvGC3uRuCAwA8.webp"
+    },
+    {
+        id: 2,
+        text: "{name}, katanya hustle culture, tapi hidup lo stuck di loading screen.",
+        meme: "https://i.giphy.com/dIV7HapCtAzHE6N3j1.webp"
+    },
+    {
+        id: 3,
+        text: "{name}, lo tuh kayak resolusi tahun baru — semangat di awal, ilang di tengah jalan.",
+        meme: "https://i.giphy.com/azUBCBFSCyNgc.webp"
+    },
+    {
+        id: 4,
+        text: "{name}, lo bilang 'trust the process'? Proses lo aja ga jelas tujuannya ke mana.",
+        meme: "https://i.giphy.com/ZZZcmyb14Ls7UQVqx0.webp"
+    },
+    {
+        id: 5,
+        text: "Gue kira lo main character, {name}, ternyata figuran numpang lewat.",
+        meme: "https://i.giphy.com/l46CwEYnbFtFfjZNS.webp"
+    },
+    {
+        id: 6,
+        text: "Katanya self growth, {name}? Tapi cara mikir lo masih stuck di era BBM.",
+        meme: "https://i.giphy.com/xJjs8eGVbjNYY.webp"
+    },
+    {
+        id: 7,
+        text: "Lo tuh kayak motivasi Senin pagi, {name} — niat doang, realisasinya 0.",
+        meme: "https://i.giphy.com/8vQSQ3cNXuDGo.webp"
+    },
+    {
+        id: 8,
+        text: "{name}, lo lebih gampang down daripada baterai HP 20%.",
+        meme: "https://i.giphy.com/rU5KhwXMUyjza.webp"
+    },
+    {
+        id: 9,
+        text: "{name}, katanya strong mentality, tapi dikit-dikit pengen quit.",
+        meme: "https://i.giphy.com/3OhXBaoR1tVPW.webp"
+    },
+    {
+        id: 10,
+        text: "{name}, lo tuh kayak rencana diet — rame diomongin, ga pernah kejadian.",
+        meme: "https://i.giphy.com/olyqByuQ9o5gY.webp"
+    },
+    {
+        id: 11,
+        text:"Lo tuh masterpiece gagal, {name}… lama dibentuk, tapi tetep bikin orang bingung.",
+        meme: "https://i.giphy.com/fPyEnsqb0Bgw8.webp"
+    }, 
+    {
+        id: 12,
+        text: "{name}, lo semangat banget, sayang hidup lo kayak Google Maps error — muter-muter ga jelas.",
+        meme: "https://i.giphy.com/GNtj4JoNwDkas.webp"
+    },
+    {
+        id: 13,
+        text: "Hidup lo tuh kayak draft tweet, {name} — banyak ide, tapi ga pernah dieksekusi.",
+        meme: "https://i.giphy.com/9PrEFkIxXUtpJRgOom.webp"
+    },
+    {
+        id: 14,
+        text: "{name}, katanya deep thinker, tapi pikiran lo lebih dangkal dari genangan abis hujan.",
+        meme: "https://i.giphy.com/xT9IgpUP5G6wQkLJ5i.webp"
+    },
+    {
+        id: 15,
+        text: "{name}, lo bilang healing trip, tapi pulang tetep bawa overthinking.",
+        meme: "https://i.giphy.com/xTiTnzGJTI3ZbL5M6c.webp"
+    }, 
+    {
+        id: 16,
+        text: "Hidup lo tuh kayak powerbank KW, {name}— niat bantu orang, tapi diri sendiri lowbat duluan.",
+        meme: "https://i.giphy.com/S6REf9iZ8SXEKhJmGJ.webp"
+    },
+    {
+        id: 17,
+        text: "Hidup {name} kayak story WA orang tua — panjang, berisik, tapi ga ada yang peduli.",
+        meme: "https://i.giphy.com/IdmfEtnMWPzOg.webp"
+    },
+    {
+        id: 18,
+        text: "Lo bangga bilang ‘trust the process’, {name}? Bro, proses lo aja ga jelas tujuannya ke mana",
+        meme: "https://i.giphy.com/c3Xv5mtcTWde8.webp"
+    }, 
+    {
+        id: 19,
+        text: "Katanya ‘menjalani hidup apa adanya’, padahal cuma alasan biar ga usaha lebih. Iya kan {name}?",
+        meme: "https://i.giphy.com/qgOlJdFGNxKrS.webp"
+    },
+    {
+        id: 20,
+        text: "{name}, lo bilang 'manifesting'? Yang ke-manifest cuma alasan sama penundaan.",
+        meme: "https://i.giphy.com/6agGMtS4rEVSo.webp"
+    }
+]
---
+++ src/main.js
@@ -0,0 +1,38 @@
+import { memes } from "./data.js"
+
+const formName = document.getElementById("name-input")
+const delayData = document.getElementById("form-input")
+
+formName.addEventListener("submit", function(e){
+    e.preventDefault()
+    const getFormData = new FormData(formName)
+    const name = getFormData.get("name")
+    
+    delayData.innerHTML = `
+        <img src="../assets/bouncing-circles.svg" class="loading"/>
+        <p id="upload-text" class="text-center">Eitsss… bentar, lagi ngulik punchline biar lo nangis.</p>
+    `
+    setTimeout(function(){
+        const uploadText = document.getElementById('upload-text')
+        uploadText.innerText = "Tunggu bentar, lagi nyari kata paling pedes 🔥"
+    }, 3000)
+
+    setTimeout(function(){
+        const rand = Math.floor(Math.random() * memes.length)
+        const memeId = memes.map(meme => meme.id)
+        const randMeme = memeId[rand]
+        const findId = memes.find(meme => meme.id === randMeme)
+        const addName = findId.text.replace("{name}", name)
+        document.body.innerHTML = `
+            <section class="w-full h-screen bg-yellow-300 flex justify-center items-center flex-col">
+                <h1 class="title-result">Panas? Biasa Aja Lah!</h1>
+                <div class="meme">
+                    <img src="${findId.meme}" class="meme-image"/>
+                    <p class="result">${addName}</p>
+                </div>
+            </section>
+        `
+
+    }, 4500)
+
+})
